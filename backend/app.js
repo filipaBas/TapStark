3db213d0125b0d88d1795ce524dbfbd5e6f54f9b
Diana Bastos <bastosdsb@gmail.com>
2024-10-20 22:50:13 +0100

    Create the /play endpoint and StarkNet provider

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const gameRoutes = require('./routes/gameRoutes');
+
+const app = express();
+const port = 5003;
+
+app.use(cors());
+
+app.use('/api', gameRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
